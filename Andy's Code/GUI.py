b52d692d38fb966fe8a7dfab483c335c80a8c916
Nomethin <106290914+Nomethin@users.noreply.github.com>
2022-12-04 15:54:14 +0800

    改了login

--- Andy's Code/GUI.py
+++ Andy's Code/GUI.py
@@ -25,20 +25,45 @@ class chatSystem:
         self.title.place(width=windowWidth, height=windowHeight/4,x=0,y=0)
 
         #button 1
-        self.hello_button = Button(parent, text="Say", command=self.say_hi, justify="center",font=("Trebuchet MS", 20, "bold"),)
-        self.hello_button.place(width = 400, height = 150, x = 0, y = 150)
+        self.login_button = Button(parent, text="Login", command=self.login, justify="center",font=("Trebuchet MS", 20, "bold"),)
+        self.login_button.place(width = 400, height = 150, x = 0, y = 150)
 
         #button 2
         self.quit_button = Button(parent, text="Quit", fg="red", command=parent.destroy,font=("Trebuchet MS", 20, "bold"),)
         self.quit_button.place(width = 400, height = 150, x = 400, y = 150)
              
+        #entry_username
+        self.entry_username = Entry(parent,font=("Trebuchet MS", 20, "bold"),)
+        self.entry_username.insert(0, 'Username')
+        self.entry_username.place(width = 400, height = 50, x = 200, y = 350)
+
+        #entry_password
+        self.entry_password = Entry(parent,font=("Trebuchet MS", 20, "bold"),)
+        self.entry_password.insert(0, 'Password')
+        self.entry_password.place(width = 400, height = 50, x = 200, y = 450)
 
         
-    def say_hi(self):
-        print("Hi there")
+    def login(self):
+        flag=0
+        f=open("login.txt","r")
+        line_list=f.readlines()
+        print(line_list)
+        data_dict={}
+        for item in line_list:
+            if item[-1] == '\n':
+                item = item[:-1]
+            temp_list=item.split(",")
+            print(temp_list)
+            data_dict[temp_list[0]]=temp_list[1]
+            print(data_dict)
+        f.close()
+        for keys,values in data_dict.items():
+            print(keys,values)
+            if keys==self.entry_username.get() and values==self.entry_password.get():
+                flag=1
 
-    def destory(self):
-        pass
+        print(flag)
+#后面如果flag==1，就进行chat，不行就重新来，输错三次重启
 
 
 def enter(event):
@@ -49,27 +74,17 @@ def exit_(event):
 
 def button1Pressed(event):
     print('Button-1 pressed at x = % d, y = % d' % (event.x, event.y))
-    mouseEffect = Canvas(root,width=500,height=500)
-
-    mouseOval1 = mouseEffect.create_oval(100,100,300,300,
-        outline="#33252f",# 边框颜色
-        fill='pink',# 填充颜色
-        width=4#
-        )
-    print(mouseOval1)
-    mouseEffect.place(x=event.x, y=event.y)
 
 
 def button3Pressed(event):
     print('Button-3 pressed at x = % d, y = % d' % (event.x, event.y))
 
-def windowResize(event):
-    settingWidth = windowWidth
-
-    if settingWidth!= root.winfo_width():
-
-        windowWidth = root.winfo_width()
-        # windowHeight = root.winfo_height()
+# def windowResize(event):
+#     settingWidth = windowWidth
+#
+#     if settingWidth!= root.winfo_width():
+#         windowWidth = root.winfo_width()
+#         # windowHeight = root.winfo_height()
 
 
 
@@ -95,6 +110,6 @@ root.bind('<Button-1>', button1Pressed)
 root.bind('<Button-3>', button3Pressed)
 root.attributes("-alpha", 0.90)#window transparency
 # root.bind('<Configure>', windowResize)
-root.resizable(0,0)#banned user zoom the window
+# root.resizable(0,0)#banned user zoom the window
 app = chatSystem(root)
 root.mainloop()
