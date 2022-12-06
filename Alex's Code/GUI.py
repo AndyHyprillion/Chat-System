0295dd08c88c26b2c5e7ce093aea0c66f6dc842a
Nomethin <106290914+Nomethin@users.noreply.github.com>
2022-12-06 12:56:51 +0800

    UI修改
    可以添加图片了

--- Alex's Code/GUI.py
+++ Alex's Code/GUI.py
@@ -1,113 +1,234 @@
-'''
-Author: AndyYe
-Date: 2022-12-01 14:49:20
-LastEditors: AndyYe
-LastEditTime: 2022-12-01 17:08:28
-FilePath: \GroupWork\GUI.py
-Description: 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 30 13:36:58 2021
 
-Copyright (c) 2022 by AndyYe, All Rights Reserved. 
-'''
+@author: bing
+"""
+
+# import all the required  modules
+import threading
+import select
 from tkinter import *
+from tkinter import font
+from tkinter import ttk
+from chat_utils import *
+import json
+
+# GUI class for the chat
 
 
-class chatSystem:
-    def __init__(self, parent):
-
-        #heading 1
-        self.title = Label(
-            parent, 
-            text="ICS Chat System",
-            font=("Trebuchet MS", 20, "bold"),
-            bg="darkblue",
-            fg = "white"
-            )
-        self.title.place(width=windowWidth, height=windowHeight/4,x=0,y=0)
-
-        #button 1
-        self.login_button = Button(parent, text="Login", command=self.login, justify="center",font=("Trebuchet MS", 20, "bold"),)
-        self.login_button.place(width = 400, height = 150, x = 0, y = 150)
-
-        #button 2
-        self.quit_button = Button(parent, text="Quit", fg="red", command=parent.destroy,font=("Trebuchet MS", 20, "bold"),)
-        self.quit_button.place(width = 400, height = 150, x = 400, y = 150)
-             
-        #entry_username
-        self.entry_username = Entry(parent,font=("Trebuchet MS", 20, "bold"),)
-        self.entry_username.insert(0, 'Username')
-        self.entry_username.place(width = 400, height = 50, x = 200, y = 350)
-
-        #entry_password
-        self.entry_password = Entry(parent,font=("Trebuchet MS", 20, "bold"),)
-        self.entry_password.insert(0, 'Password')
-        self.entry_password.place(width = 400, height = 50, x = 200, y = 450)
-
-        self.login_dict={}
+class GUI:
+    # constructor method
+    def __init__(self, send, recv, sm, s):
+        # chat window which is currently hidden
+        self.Window = Tk()
+        self.Window.withdraw()
+        self.send = send
+        self.recv = recv
+        self.sm = sm
+        self.socket = s
+        self.my_msg = ""
+        self.system_msg = ""
+        self.image=PhotoImage(file="pict_1.png") #只需要改image路径即可
 
     def login(self):
-        self.login_dict={self.entry_username.get():self.entry_password.get()}
-        flag=0
-        f=open("login.txt","r")
-        line_list=f.readlines()
-        data_dict={}
-        for item in line_list:
-            if item[-1] == '\n':
-                item = item[:-1]
-            temp_list=item.split(",")
-            data_dict[temp_list[0]]=temp_list[1]
-        f.close()
-        for keys,values in data_dict.items():
-            if keys==self.entry_username.get() and values==self.entry_password.get():
-                flag=1
+        # login window
+        self.login = Toplevel()
+        # set the title
+        self.login.title("Welcome to our chat system!")
+        self.login.resizable(width=True,
+                             height=True)
+        self.login.configure(width=800,
+                             height=600)
+        # create a Label
+        self.welcome = Label(self.login,
+                         image=self.image,
+                         text="Please login to continue",
+                         justify=CENTER,
+                         font="Helvetica 14 bold")
 
-        print(flag)
-#后面如果flag==1，就进行chat，不行就重新来，输错三次重启
+        self.welcome.place(relheight=0.2,
+                       relx=0.2,
+                       rely=0.07)
+        # create a Label
+        self.label_user_name = Label(self.login,
+                               text="User name: ",
+                               font="Helvetica 12")
+
+        self.label_user_name.place(relheight=0.1,
+                             relx=0.1,
+                             rely=0.45)
+
+        # create a entry box for
+        # tyoing the message
+        self.entryName = Entry(self.login,
+                               font="Helvetica 14")
+
+        self.entryName.place(relwidth=0.4,
+                             relheight=0.1,
+                             relx=0.35,
+                             rely=0.45)
+
+        # set the focus of the curser
+        self.entryName.focus()
+
+        self.go = Button(self.login,
+                         text="CONTINUE",
+                         font="Helvetica 14 bold",
+                         command=lambda: self.goAhead(self.entryName.get()))
+
+        self.go.place(relx=0.4,
+                      rely=0.8)
+        self.Window.mainloop()
+
+    def goAhead(self, name):
+        if len(name) > 0:
+            msg = json.dumps({"action": "login", "name": name})
+            self.send(msg)
+            response = json.loads(self.recv())
+            if response["status"] == 'ok':
+                self.login.destroy()
+                self.sm.set_state(S_LOGGEDIN)
+                self.sm.set_myname(name)
+                self.layout(name)
+                self.textCons.config(state=NORMAL)
+                # self.textCons.insert(END, "hello" +"\n\n")
+                self.textCons.insert(END, menu + "\n\n")
+                self.textCons.config(state=DISABLED)
+                self.textCons.see(END)
+                # while True:
+                #     self.proc()
+        # the thread to receive messages
+            process = threading.Thread(target=self.proc)
+            process.daemon = True
+            process.start()
+
+#----------------------------------------------------------------------
+
+    # The main layout of the chat
+    def layout(self, name):
+
+        self.name = name
+        # to show chat window
+        self.Window.deiconify()
+        self.Window.title("CHATROOM")
+        self.Window.resizable(width=False,
+                              height=False)
+        self.Window.configure(width=470,
+                              height=550,
+                              bg="#17202A")
+        self.labelHead = Label(self.Window,
+                               bg="#17202A",
+                               fg="#EAECEE",
+                               text=self.name,
+                               font="Helvetica 13 bold",
+                               pady=5)
+
+        self.labelHead.place(relwidth=1)
+        self.line = Label(self.Window,
+                          width=450,
+                          bg="#ABB2B9")
+
+        self.line.place(relwidth=1,
+                        rely=0.07,
+                        relheight=0.012)
+
+        self.textCons = Text(self.Window,
+                             width=20,
+                             height=2,
+                             bg="#17202A",
+                             fg="#EAECEE",
+                             font="Helvetica 14",
+                             padx=5,
+                             pady=5)
+
+        self.textCons.place(relheight=0.745,
+                            relwidth=1,
+                            rely=0.08)
+
+        self.labelBottom = Label(self.Window,
+                                 bg="#ABB2B9",
+                                 height=80)
+
+        self.labelBottom.place(relwidth=1,
+                               rely=0.825)
+
+        self.entryMsg = Entry(self.labelBottom,
+                              bg="#2C3E50",
+                              fg="#EAECEE",
+                              font="Helvetica 13")
+
+        # place the given widget
+        # into the gui window
+        self.entryMsg.place(relwidth=0.74,
+                            relheight=0.06,
+                            rely=0.008,
+                            relx=0.011)
+
+        self.entryMsg.focus()
+
+        # create a Send Button
+        self.buttonMsg = Button(self.labelBottom,
+                                text="Send",
+                                font="Helvetica 10 bold",
+                                width=20,
+                                bg="#ABB2B9",
+                                command=lambda: self.sendButton(self.entryMsg.get()))
+
+        self.buttonMsg.place(relx=0.77,
+                             rely=0.008,
+                             relheight=0.06,
+                             relwidth=0.22)
+
+        self.textCons.config(cursor="arrow")
+
+        # create a scroll bar
+        scrollbar = Scrollbar(self.textCons)
+
+        # place the scroll bar
+        # into the gui window
+        scrollbar.place(relheight=1,
+                        relx=0.974)
+
+        scrollbar.config(command=self.textCons.yview)
+
+        self.textCons.config(state=DISABLED)
+
+    # function to basically start the thread for sending messages
+
+    def sendButton(self, msg):
+        # self.textCons.config(state=DISABLED)
+        self.my_msg = msg
+        # print(msg)
+        self.entryMsg.delete(0, END)
+        self.textCons.config(state=NORMAL)
+        self.textCons.insert(END, msg + "\n")
+        self.textCons.config(state=DISABLED)
+        self.textCons.see(END)
+
+    def proc(self):
+        # print(self.msg)
+        while True:
+            read, write, error = select.select([self.socket], [], [], 0)
+            peer_msg = []
+            # print(self.msg)
+            if self.socket in read:
+                peer_msg = self.recv()
+            if len(self.my_msg) > 0 or len(peer_msg) > 0:
+                # print(self.system_msg)
+                self.system_msg = self.sm.proc(self.my_msg, peer_msg)
+                self.my_msg = ""
+                self.textCons.config(state=NORMAL)
+                self.textCons.insert(END, self.system_msg + "\n\n")
+                self.textCons.config(state=DISABLED)
+                self.textCons.see(END)
+
+    def run(self):
+        self.login()
 
 
-def enter(event):
-    print('Mouse Enter the Window')
-
-def exit_(event):
-    print('Mouse Leave the Window')
-
-def button1Pressed(event):
-    print('Button-1 pressed at x = % d, y = % d' % (event.x, event.y))
-
-
-def button3Pressed(event):
-    print('Button-3 pressed at x = % d, y = % d' % (event.x, event.y))
-
-# def windowResize(event):
-#     settingWidth = windowWidth
-#
-#     if settingWidth!= root.winfo_width():
-#         windowWidth = root.winfo_width()
-#         # windowHeight = root.winfo_height()
-
-
-
-
-root = Tk()
-
-ws = root.winfo_screenwidth()
-hs = root.winfo_screenheight()
-windowWidth = 800
-windowHeight = 600
-x = (ws/2) - (windowWidth/2)
-y = (hs/2) - (windowHeight/2)
-
-#setting window size & position
-root.geometry("%dx%d+%d+%d" % (windowWidth,windowHeight,x,y))#width x height + startX + startY
-
-#window title
-root.title("ICS Chat System")
-
-root.bind('<Enter>', enter)
-root.bind('<Leave>', exit_)
-root.bind('<Button-1>', button1Pressed)
-root.bind('<Button-3>', button3Pressed)
-root.attributes("-alpha", 0.90)#window transparency
-# root.bind('<Configure>', windowResize)
-# root.resizable(0,0)#banned user zoom the window
-app = chatSystem(root)
-root.mainloop()
+# create a GUI class object
+if __name__ == "__main__":
+    # g = GUI()
+    pass
