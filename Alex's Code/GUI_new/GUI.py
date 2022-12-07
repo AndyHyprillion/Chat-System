eab00e82ce302946b2487fc6f250685260d8795e
Nomethin <106290914+Nomethin@users.noreply.github.com>
2022-12-07 08:57:16 +0800

    写了update_data函数，但是好像有点问题

--- Alex's Code/GUI_new/GUI.py
+++ Alex's Code/GUI_new/GUI.py
@@ -14,6 +14,7 @@ from tkinter import font
 from tkinter import ttk
 from chat_utils import *
 import json
+import random
 
 # GUI class for the chat
 
@@ -31,6 +32,7 @@ class GUI:
         self.my_msg = ""
         self.system_msg = ""
         self.image=PhotoImage(file="pict_1.png") #改这个路径即可
+        
 
     #加入了账号密码判断,已经实现了只有在txt中的账号密码可以登录
     def login_flag(self):
@@ -115,32 +117,158 @@ class GUI:
 
         #第二个entry
         self.entry_password = Entry(self.login,
-                               font=("Trebuchet MS",12))
+                               font=("Trebuchet MS",12),
+                               show="*")
 
         self.entry_password.place(relwidth=0.30,
                              relheight=0.05,
                              relx=0.4,
-                             rely=0.75)
+                             rely=0.75,
+                             )
 
         self.entry_password.focus()
-
-        #这个是register button
-        self.register=Button(self.login,
-                              text="Register",
-                              font=("Trebuchet MS", 14, "bold"),
-                              )
-        self.register.place(relx=0.55,
-                            rely=0.85)
 
         #这个是continue button
         self.go = Button(self.login,
                          text="Login",
                          font=("Trebuchet MS", 14, "bold"),
-                         command=lambda: self.goAhead(self.entry_username.get()))
+                        #  command= self.goAhead(self.entry_username.get())
+                        command= self.pressLogin
+                         )
 
         self.go.place(relx=0.25,
                       rely=0.85)
+
+        #这个是register button
+        self.register=Button(self.login,
+                              text="Register",
+                              font=("Trebuchet MS", 14, "bold"),
+                              command=self.reg)
+
+        self.register.place(relx=0.55,
+                            rely=0.85)
+        
         self.Window.mainloop()
+
+    def pressLogin(self):
+        self.goAhead(self.entry_username.get())
+        print(1111)
+
+
+    def reg(self):
+
+        self.reg = Toplevel()
+        
+        ws = self.reg.winfo_screenwidth()
+        hs = self.reg.winfo_screenheight()
+        windowWidth = 400
+        windowHeight = 300
+        x = (ws/2) - (windowWidth/2)
+        y = (hs/2) - (windowHeight/2)
+
+        #setting window size & position
+        self.reg.geometry("%dx%d+%d+%d" % (windowWidth,windowHeight,x,y))#width x height + startX + startY
+        print("here")
+
+        # create a Label
+        self.label_set_username = Label(self.reg,
+                               text="Username",
+                               font=("Trebuchet MS", 12),
+                               textvariable=self.entry_username.get())
+
+        self.label_set_username.focus()
+
+        self.label_set_username.place(relheight=0.1,
+                             relx=0.1,
+                             rely=0.1)
+
+        # create a entry box for
+        # typing the message
+        self.entry_set_username = Entry(self.reg,
+                               font=("Trebuchet MS",12),
+                               )
+
+        self.entry_set_username.place(relwidth=0.60,
+                             relheight=0.1,
+                             relx=0.3,
+                             rely=0.1,
+                             )
+
+
+        #第二个label
+        self.label_set_password = Label(self.reg,
+                               text="Password",
+                               font=("Trebuchet MS",12))
+
+        self.label_set_password.place(relheight=0.1,
+                             relx=0.1,
+                             rely=0.3)
+
+        #第二个entry
+        self.entry_set_password = Entry(self.reg,
+                               font=("Trebuchet MS",12))
+
+        self.entry_set_password.place(relwidth=0.60,
+                             relheight=0.1,
+                             relx=0.3,
+                             rely=0.3)
+
+        self.entry_set_password.focus()
+
+        #第3个label
+        self.label_again_password = Label(self.reg,
+                               text="Confirm",
+                               font=("Trebuchet MS",12))
+
+        self.label_again_password.place(relheight=0.1,
+                             relx=0.1,
+                             rely=0.5)
+
+        #第3个entry
+        self.entry_again_password = Entry(self.reg,
+                               font=("Trebuchet MS",12))
+
+        self.entry_again_password.place(relwidth=0.60,
+                             relheight=0.1,
+                             relx=0.3,
+                             rely=0.5)
+
+        #下面的confirm button
+        self.register_confirm=Button(self.reg,
+                              text="Confirm",
+                              font=("Trebuchet MS", 14, "bold"),
+                              command=self.update_data())
+
+        self.register_confirm.place(relx=0.45,
+                                    rely=0.75)
+
+    #奇怪的是，它更新不下去，但它仍然能在文档中打出一个逗号，看来是get entry的问题
+    def update_data(self):
+        if self.entry_set_password.get()==self.entry_again_password.get():
+            flag_for_username = 0
+            f = open("login.txt", "r")
+            line_list = f.readlines()
+            data_dict = {}
+            for item in line_list:
+                if item[-1] == '\n':
+                    item = item[:-1]
+                temp_list = item.split(",")
+                data_dict[temp_list[0]] = temp_list[1]
+            f.close()
+
+            for keys in data_dict.keys():
+                if keys == self.entry_set_username.get():
+                    flag_for_username = 1
+
+            if flag_for_username != 1:
+                f = open("login.txt", "a")
+                f.write(self.entry_set_username.get() + "," + self.entry_set_password.get() + "\n")
+                f.close()
+
+            else:
+                print("please change the username")
+        else:
+            print("please check your password")
 
     def goAhead(self, name):
         if len(name) > 0 and self.login_flag()==1:
